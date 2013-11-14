3a67c92cc7411e14400cb9ee1ac51b10c1171c0d
sintell <uyiiio@gmail.com>
2013-11-14 13:01:25 +0400

    Fixes

--- app/views/dishes/new-dish.blade.php
+++ app/views/dishes/new-dish.blade.php
@@ -6,7 +6,7 @@
 <label for="" class="form__label">Название блюда</label><input type="text" placeholder="Название блюда" name="name" required>
 <label for="" class="form__label">Тип</label><input type="text" placeholder="Тип (первое, второе...)" name="type" required>
 <label for="" class="form__label">Цена</label><input type="number" placeholder="Цена блюда" name="price" required></label> 
-    <input type="submit" value="Добавить">
+    <input type="submit" value="Добавить"> 
   </form>
 
 @stop
